6c5dabe7dc0fb25e265e5a825d9fb0c6817a5002
dylan-cancelliere <dycancel@gmail.com>
2020-12-25 10:13:55 -0500

    Setup rng, fonts, ROM loading

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # chip8
 Basic CHIP-8 emulator in C++ to learn more about emulators
+See [here](https://austinmorlan.com/posts/chip8_emulator/) for the writeup I'm following.
--- chip8.cpp
+++ chip8.cpp
@@ -1,6 +1,15 @@
 #include <cstdint>
+#include <fstream>
 
 class Chip8{
+  Chip8():randGen(std::chrono::system_clock::now().time_since_epoch().count()){
+    // Init RNG
+    randByte = std::uniform_int_distribution<uint8_t>(0,255U);
+  }
+
+  std::default_random_engine randgen;
+  std::uniform_int_distribution<uint8_t> randByte;
+  
 public:
   uint8_t registers[16]{};
   uint8_t memory[4096]{};
@@ -14,3 +23,59 @@ public:
   uint32_t video[64*32]{};
   uint16_t opcode;
 };
+
+const unsigned int START_ADDRESS = 0x200;
+
+void Chip8::LoadROM(char const* filename){
+  // Open file as binary stream and move file pointer to the end
+ std:ifstream file(filename, std::ios::binary | std::ios::ate);
+
+  if (file.is_open()){
+    // Get file size and allocate a buffer to hold the data
+    std::streampos size = file.tellg();
+    char* buffer = new char[size];
+
+    // Go back to the beginning of the file and fill the buffer
+    file.seekg(0, std::ios::beg);
+    file.read(buffer, size);
+    file.close();
+
+    // Load the ROM into memory
+    for (long i = 0; i < size; i++){
+      memory[START_ADDRESS + i] = buffer[i];
+    }
+
+    delete[] buffer;
+  }
+}
+
+const unsinged int FONTSET_SIZE = 80;
+
+uint8_t fontset[FONTSET_SIZE] = {
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+Chip8::Chip8(){
+  // Init PC
+  pc = START_ADDRESS;
+
+  // load fonts into memory
+  for (unsinged int i = 0; i < FONTSET_SIZE; i++){
+    memory[FONTSET_START_ADDRESS + i] = fontset[i];
+  }
+}
